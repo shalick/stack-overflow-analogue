914da74f01c9c173c4f46b45956e911070519ff4
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2025-06-23 16:46:51 +0300

    chore: add api and axios

---
+++ src/api/axios.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api",
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    if (error.response?.status === 401) {
+      try {
+        await api.post("/refresh");
+        return api(error.config);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
